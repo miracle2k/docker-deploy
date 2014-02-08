5b62424ea762e543f069d420787951136a5be2d9
Michael Elsdörfer <michael@elsdoerfer.com>
2014-02-08 12:38:32 +0100

    Execute Bootstrap file sequentially.
    This is the easiest way to fix the setup issues for now. Later, we
    would have to a) declare --links b) make services like shelf service
    discover restart.

--- deploylib.py
+++ deploylib.py
@@ -18,6 +18,54 @@ def directory(path):
     os.chdir(old)
 
 
+import yaml
+import yaml.constructor
+
+try:
+    # included in standard lib from Python 2.7
+    from collections import OrderedDict
+except ImportError:
+    # try importing the backported drop-in replacement
+    # it's available on PyPI
+    from ordereddict import OrderedDict
+
+class OrderedDictYAMLLoader(yaml.Loader):
+    """
+    A YAML loader that loads mappings into ordered dictionaries.
+    """
+
+    def __init__(self, *args, **kwargs):
+        yaml.Loader.__init__(self, *args, **kwargs)
+
+        self.add_constructor(u'tag:yaml.org,2002:map', type(self).construct_yaml_map)
+        self.add_constructor(u'tag:yaml.org,2002:omap', type(self).construct_yaml_map)
+
+    def construct_yaml_map(self, node):
+        data = OrderedDict()
+        yield data
+        value = self.construct_mapping(node)
+        data.update(value)
+
+    def construct_mapping(self, node, deep=False):
+        if isinstance(node, yaml.MappingNode):
+            self.flatten_mapping(node)
+        else:
+            raise yaml.constructor.ConstructorError(None, None,
+                'expected a mapping node, but found %s' % node.id, node.start_mark)
+
+        mapping = OrderedDict()
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError, exc:
+                raise yaml.constructor.ConstructorError('while constructing a mapping',
+                    node.start_mark, 'found unacceptable key (%s)' % exc, key_node.start_mark)
+            value = self.construct_object(value_node, deep=deep)
+            mapping[key] = value
+        return mapping
+
+
 class Service(dict):
 
     def __init__(self, name, data):
@@ -79,16 +127,19 @@ class Service(dict):
 
     def path(self, p):
         """Make the given path absolute."""
-        return abspath(path(dirname(self.from_file.filename), p))
+        return self.from_file.path(p)
 
 
 class ServiceFile(dict):
     """A file listing multiple services."""
 
     @classmethod
-    def load(cls, filename):
+    def load(cls, filename, ordered=False):
         with open(filename, 'r') as f:
-            structure = yaml.load(f)
+            opts = {}
+            if ordered:
+                opts={'Loader': OrderedDictYAMLLoader}
+            structure = yaml.load(f, **opts)
 
         servicefile = cls()
         servicefile.filename = filename
