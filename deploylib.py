8e5a264bd9d19327ac5ac19e32af8ade1a0c9373
Michael Elsdörfer <michael@elsdoerfer.com>
2014-04-26 12:47:07 +0200

    sdutil syntax has changed.

--- deploylib.py
+++ deploylib.py
@@ -375,7 +375,7 @@ class Host(object):
         if 'register' in service:
             cmd = service.cmd
             for port, pname in service.register.items():
-                cmd = '/sdutil exec -i eth0 {did}:{sname}:{pname}:{p} {cmd}'.format(
+                cmd = '/sdutil exec -i eth0 -s {did}:{sname}:{pname}:{p} {cmd}'.format(
                     did=deploy_id, sname=service.name, pname=pname, p=port, cmd=cmd)
             service.cmd = cmd[len('/sdutil '):]
             service.entrypoint = '/sdutil'
