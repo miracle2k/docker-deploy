9475efcb94e12e9b2e5eec9bcac659f6e347197a
Michael Elsdörfer <michael@elsdoerfer.com>
2014-05-25 22:03:19 +0200

    Start with two plugins that the future should bring.

---
+++ deploylib/plugins/passwords.py
@@ -0,0 +1,8 @@
+"""Should supports generating passwords to be used::
+
+    Passwords:
+        DatabasePassword:
+            - length: 15
+
+These will be made available as local and env variables.
+"""
---
+++ deploylib/plugins/vars.py
@@ -0,0 +1,20 @@
+"""Supports defining variables, more appropriately called constants::
+
+    Vars:
+        ServiceName: foo
+
+TODO: Currently this plugin is disabled, because we need to think about the
+design of variables more. It is currently not clear where and when what
+is supposed to be replaced, and in particular how this should work for plugins
+that run outside the "deploy container" code where the replacement vars
+are currently being put together.
+"""
+
+from deploylib.plugins import Plugin
+
+
+class VarsPlugin(Plugin):
+
+    def provide_local_vars(self, service, vars):
+        for key, item in service.globals.get('Vars', {}).items():
+            vars[key] = item.format(**vars)
